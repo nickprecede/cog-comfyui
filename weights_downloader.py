3e1b501cedc7ac58a3be219ac45ed25a1b4432ce
nickprecede <161537092+nickprecede@users.noreply.github.com>
2024-11-14 11:46:30 -0800

    Print download commands

--- weights_downloader.py
+++ weights_downloader.py
@@ -71,6 +71,7 @@ class WeightsDownloader:
 
         print(f"⏳ Downloading {weight_str} to {dest}")
         start = time.time()
+        print(' '.join(["pget", "--log-level", "warn", "-xf", url, dest]))
         subprocess.check_call(
             ["pget", "--log-level", "warn", "-xf", url, dest], close_fds=False
         )
